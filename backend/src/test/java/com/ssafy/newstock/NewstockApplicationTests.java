f5590a9ab7b3bf5e48b9fd0f4f58807a35fbd90d
이주희 <leejh0799@gmail.com>
2024-09-05 14:05:48 +0900

    프로젝트 생성

---
+++ backend/src/test/java/com/ssafy/newstock/NewstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.newstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
