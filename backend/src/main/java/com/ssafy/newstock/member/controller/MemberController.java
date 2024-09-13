c71f02598a6ecea8088125731daf5d6dfa87728e
이주희 <leejh0799@gmail.com>
2024-09-13 13:42:49 +0900

    비밀번호 재발급 기능 구현

--- backend/src/main/java/com/ssafy/newstock/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/newstock/member/controller/MemberController.java
@@ -2,13 +2,14 @@ package com.ssafy.newstock.member.controller;
 
 import com.ssafy.newstock.member.controller.request.SignUpRequest;
 import com.ssafy.newstock.member.service.MemberService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +20,27 @@ public class MemberController {
     public ResponseEntity<Void> signUp(@Valid @RequestBody SignUpRequest request) {
         memberService.signUp(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/check-email")
+    public boolean checkEmail(@RequestParam String email) {
+        return memberService.isEmailDuplicated(email);
+    }
+
+    @GetMapping("/check-nickname")
+    public boolean checkNickname(@RequestParam String nickname) {
+        return memberService.isNicknameDuplicated(nickname);
+    }
+
+    @PostMapping("/send-email")
+    public void sendPasswordEmail(@RequestParam("email") String email) {
+        memberService.sendPasswordEmail(email);
+    }
+
+    @GetMapping("/reset-password")
+    public void resetPassword(@RequestParam("email") String email, @RequestParam("id") String id,
+                              HttpServletResponse response) throws IOException {
+        memberService.resetPassword(email, id);
+        response.sendRedirect("/reset-password-success");
     }
 }
