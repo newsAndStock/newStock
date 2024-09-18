45d8a1da1a3a1f9aababa64ee5fa4d33261d2ad9
정지원 <z1gram@naver.com>
2024-09-19 01:11:57 +0900

    feat: 메인페이지 UI 제작

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:frontend/screens/loading_screen.dart';
 
 void main() async {
-  await dotenv.load(fileName: "assets/env/.env");
+  // await dotenv.load(fileName: "assets/env/.env");
   runApp(const App());
 }
 
