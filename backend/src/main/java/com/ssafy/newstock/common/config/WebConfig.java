fc80eba9f4b2ca0c7a74a8c8377dde59361c6e91
강도원 <3120kang@naver.com>
2024-09-24 09:09:34 +0900

    BE: 테스트 했던 코드 복원

--- backend/src/main/java/com/ssafy/newstock/common/config/WebConfig.java
+++ backend/src/main/java/com/ssafy/newstock/common/config/WebConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
-@ComponentScan
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
