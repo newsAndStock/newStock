a7eadf47eee936f0736f186a39a14fcaa5655aa1
강도원 <3120kang@naver.com>
2024-09-26 17:39:44 +0900

    BE: 진행중인 거래 조회

--- backend/src/main/java/com/ssafy/newstock/trading/repository/TradingRepository.java
+++ backend/src/main/java/com/ssafy/newstock/trading/repository/TradingRepository.java
@@ -1,9 +1,20 @@
 package com.ssafy.newstock.trading.repository;
 
+import com.ssafy.newstock.trading.domain.OrderType;
+import com.ssafy.newstock.trading.domain.TradeType;
 import com.ssafy.newstock.trading.domain.Trading;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TradingRepository extends JpaRepository<Trading,Long> {
 
+    List<Trading> findByMemberIdAndIsCompletedFalse(Long memberId);
+
+    // 매수중인 거래 조회
+    List<Trading> findByMemberIdAndIsCompletedFalseAndOrderType(Long memberId, OrderType orderType);
+
+    // 매도중인 거래 조회
+    List<Trading> findByMemberIdAndIsCompletedFalseAndTradeType(Long memberId, OrderType orderType);
 
 }
