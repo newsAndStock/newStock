21aa5d41e6f365cd9b64937b1a83b09cde4c9153
이주희 <leejh0799@gmail.com>
2024-10-03 22:34:15 +0900

    BE: 비밀번호 재설정 요청 도메인 주소 수정

--- backend/src/main/java/com/ssafy/newstock/external/EmailSender.java
+++ backend/src/main/java/com/ssafy/newstock/external/EmailSender.java
@@ -4,6 +4,7 @@ import com.ssafy.newstock.external.exception.EmailSendFailException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -19,6 +20,9 @@ public class EmailSender {
     private final JavaMailSender javaMailSender;
     private final TemplateEngine templateEngine;
 
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+
     public void sendTemporaryPassword(String email, String id, String temporaryPassword) {
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 
@@ -30,6 +34,7 @@ public class EmailSender {
 
         try {
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+            mimeMessageHelper.setFrom(senderEmail);
             mimeMessageHelper.setTo(email);
             mimeMessageHelper.setSubject("[NEWSTOCK] 임시 비밀번호입니다");
             mimeMessageHelper.setText(emailContent, true);
