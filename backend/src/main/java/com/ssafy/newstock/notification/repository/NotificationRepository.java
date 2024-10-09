6ce551b34129f1dc61cec0a53d3cb29baca4d1a6
강도원 <3120kang@naver.com>
2024-10-09 21:48:47 +0900

    BE: 알람 최신순으로 조회하도록 수정

--- backend/src/main/java/com/ssafy/newstock/notification/repository/NotificationRepository.java
+++ backend/src/main/java/com/ssafy/newstock/notification/repository/NotificationRepository.java
@@ -7,5 +7,6 @@ import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification,Long> {
 
-    List<Notification> findAllByReceiverId(Long memberId);
+    List<Notification> findAllByReceiverIdOrderByCreatedAtDesc(Long memberId);
+
 }
